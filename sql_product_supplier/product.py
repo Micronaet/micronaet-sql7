0651728a2da8268229111c8575117189d41f0ebf
nicothebrush <nicola.riolini@gmail.com>
2017-04-05 13:22:23 +0200

    fast git

--- sql_product_supplier/product.py
+++ sql_product_supplier/product.py
@@ -100,8 +100,8 @@ class product_product(osv.osv):
         else:
             context = args.get('context', {})
          
-        res = True#super(product_product, self).schedule_sql_product_import(
-        #    cr, uid, **args)
+        res = super(product_product, self).schedule_sql_product_import(
+            cr, uid, **args)
 
         _logger.info('Start update product supplier!')
         
